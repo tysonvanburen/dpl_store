1856c2fc78c6bc32083a4696afa5755d601142cc
DevPoint Labs <DPL1@devpointsimac3.domain.actdsltmp>
2014-06-12 10:12:55 -0600

    modified seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,2 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+Product.create(name: 'DPL Shirt', price: 14.99, sku: 'dpl123', description: 'An awesome purple shirt with the DPL logo')
+Product.create(name: 'DPL Mug', price: 3.25, sku: 'dpl456', description: 'An awesome purple mug with the DPL logo')
