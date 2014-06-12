e6351768d366df2722474607ea800973487385bc
DevPoint Labs <DPL1@devpointsimac3.domain.actdsltmp>
2014-06-12 14:22:18 -0600

    product_params to save information

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,3 +1,36 @@
 class ProductsController < ApplicationController
-  
+
+  # The index action
+  # This should list all of our products
+  # index.html.erb
+  def index
+    @products = Product.all
+  end
+
+  # This should find a particular product
+  # show.html.erb
+  def show
+    @product = Product.find(params[:id])
+  end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to products_path
+    else
+      render :new
+    end
+  end
+
+  private
+  # define a private method
+  # use strong parameters
+    def product_params
+      params.require(:product).permit(:name)
+    end
+
 end
