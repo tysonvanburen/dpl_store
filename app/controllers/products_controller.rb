47d06a59c22413d4edf2389552e474bdb96c0b2a
DevPoint Labs <DPL1@devpointsimac3.domain.actdsltmp>
2014-06-13 12:05:21 -0600

    oops. haven't commited in a bit. added a bunch of stuff. U'm a bad developer

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,4 +1,6 @@
 class ProductsController < ApplicationController
+  # before hook
+  before_action :find_product, only: [:show, :edit, :update, :destroy]
 
   # The index action
   # This should list all of our products
@@ -10,27 +12,47 @@ class ProductsController < ApplicationController
   # This should find a particular product
   # show.html.erb
   def show
-    @product = Product.find(params[:id])
   end
 
   def new
     @product = Product.new
   end
 
+  def edit
+  end
+
+  def update
+    if @product.update_attributes(product_params)
+    redirect_to products_path
+  else
+    render :edit
+  end
+  end
+
   def create
     @product = Product.new(product_params)
     if @product.save
+      flash[:notice] = 'Very Good Young Grasshopper!'
       redirect_to products_path
     else
+      flash[:alert] = 'Very Bad Young Grasshopper!'
       render :new
     end
   end
 
+  def destroy
+    @product.destroy
+    redirect_to products_path
+  end
   private
   # define a private method
   # use strong parameters
     def product_params
       params.require(:product).permit(:name)
+    end
+
+    def find_product
+      @product = Product.find(params[:id])
     end
 
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+
+  def show_notice_message
+      unless flash.empty?
+        [:notice, :alert].each do |key|
+          return content_tag(:div, flash[key], class: "flash-#{key}") if flash[key].present?
+      end
+    end
+  end
+
 end
